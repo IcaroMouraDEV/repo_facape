07608fb61bd0b0912d5572d4dc17de7b78841250
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-08 11:44:32 -0300

    feat: implement calc_salary_increase function

---
+++ calc_salary_increase.py
@@ -0,0 +1,10 @@
+def calc_salary_increase():
+    salary = float(input('Informe o seu salario: '))
+
+    if salary > 1250:
+        print(f'seu novo salario é {salary * 0.1 + salary}')
+    else:
+        print(f'seu novo salario é {salary * 0.15 + salary}')
+
+
+calc_salary_increase()
