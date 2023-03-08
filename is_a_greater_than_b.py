ccacf1bd32f946fc20925af057619b38b7583d70
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-08 11:10:27 -0300

    feat: implement is_new function

--- is_a_greater_than_b.py
+++ is_a_greater_than_b.py
@@ -3,11 +3,11 @@ def is_a_greater_than_b() -> None:
     num2  = int(input('digite o segundo numero: '))
 
     if num > num2:
-        print(f'''{num} > {num2}''')
+        print(f'{num} é maior que {num2}')
     elif num < num2:
-        print(f'''{num} < {num2}''')
+        print(f'{num} é menor que {num2}')
     else:
-        print(f'''{num} = {num2}''')        
+        print(f'{num} é igual a {num2}')        
 
 
 is_a_greater_than_b()
---
+++ is_new.py
@@ -0,0 +1,7 @@
+def is_new():
+    age: input('Digite a idade do carro: ')
+
+    if age <= 3:
+        print('O carro é novo')
+    else:
+        print('O carro é velho')
